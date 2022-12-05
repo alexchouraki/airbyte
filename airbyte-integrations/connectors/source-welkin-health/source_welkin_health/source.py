24aeef948432169eb62630a6cdddc151e649bedc
alexchouraki <alexchouraki@me.com>
2022-12-05 15:32:35 +0100

    renaming streams ; support of incremental streams

--- airbyte-integrations/connectors/source-welkin-health/source_welkin_health/source.py
+++ airbyte-integrations/connectors/source-welkin-health/source_welkin_health/source.py
@@ -12,20 +12,12 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from airbyte_cdk.sources.streams import IncrementalMixin
 
 
 """
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
+Add integration tests and unit tests.
+Open-source this connector.
 """
 
 class BaseWelkinHealthStream(HttpStream, ABC):
@@ -34,36 +26,11 @@ class BaseWelkinHealthStream(HttpStream, ABC):
         self._start_date = start_date
         self._tenant_name = tenant_name
         self._instance_name = instance_name
+        self._cursor_value = None
 
 
 # Basic full refresh stream
 class WelkinHealthStream(BaseWelkinHealthStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class WelkinHealthStream(HttpStream, ABC)` which is the current class
-    `class Customers(WelkinHealthStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(WelkinHealthStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalWelkinHealthStream((WelkinHealthStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
-
     primary_key = "id"
     page_size = 100
 
@@ -72,20 +39,41 @@ class WelkinHealthStream(BaseWelkinHealthStream, ABC):
         return f"https://api.live.welkincloud.io/{self._tenant_name}/{self._instance_name}/"
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+# Basic incremental stream
+class IncrementalWelkinHealthStream(WelkinHealthStream, IncrementalMixin, ABC):
+
+    state_checkpoint_interval = 500
+    
+    @property
+    def state(self) -> Mapping[str, Any]:
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value}
+        else:
+            return {self.cursor_field: self.start_date}
+    
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+       self._cursor_value = value[self.cursor_field]
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
         """
+        latest_state = latest_record.get(self.cursor_field)
+        current_state = current_stream_state.get(self.cursor_field) or latest_state
+        if latest_state:
+            return {self.cursor_field: max(latest_state, current_state)}
+        else:
+            return {self.cursor_field: current_state}
+
+class ExportIncrementalWelkinHealthStream(IncrementalWelkinHealthStream, ABC):
+    primary_key = "id"
+    cursor_field = "updatedAt"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         try:
             if response.json()['cursor']['hasNext']:
                 return {"start": response.json()['cursor']['end']}
@@ -97,94 +85,72 @@ class WelkinHealthStream(BaseWelkinHealthStream, ABC):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
         if next_page_token is not None:
             return {"start": next_page_token['start']}
+        elif self._cursor_value:
+            return {"start": self._cursor_value}
         else:
-            return {}
+            return {"start": self._start_date}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         data = response.json()['data']
         yield from [record for record in data]
 
-class ExportAssessments(WelkinHealthStream):
-    primary_key = "id"
-
+class Assessments(ExportIncrementalWelkinHealthStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/ASSESSMENT"
 
-class ExportCalendarEvents(WelkinHealthStream):
-    primary_key = "id"
+class CalendarEvents(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/CALENDAR_EVENT"
 
-class ExportCdtRecords(WelkinHealthStream):
-    primary_key = "id"
-
+class CdtRecords(ExportIncrementalWelkinHealthStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/CDT_RECORD"
 
-class ExportEmails(WelkinHealthStream):
-    primary_key = "id"
+class Emails(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/EMAIL"
 
-class ExportEncounters(WelkinHealthStream):
-    primary_key = "id"
+class Encounters(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/ENCOUNTER"
 
-class ExportEncounterComments(WelkinHealthStream):
-    primary_key = "id"
+class EncounterComments(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/ENCOUNTER_COMMENT"
 
-class ExportEncounterDispositions(WelkinHealthStream):
-    primary_key = "id"
+class EncounterDispositions(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/ENCOUNTER_DISPOSITION"
 
-class ExportPatients(WelkinHealthStream):
-    primary_key = "id"
+class Patients(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/PATIENT"
 
-class ExportPfas(WelkinHealthStream):
-    primary_key = "id"
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return "export/PFA"
-
-
-class ExportSms(WelkinHealthStream):
-    primary_key = "id"
+class Sms(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -192,23 +158,21 @@ class ExportSms(WelkinHealthStream):
         return "export/SMS"
 
 
-class ExportTasks(WelkinHealthStream):
-    primary_key = "id"
+class Tasks(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/TASK"
 
-class ExportVoiceCalls(WelkinHealthStream):
-    primary_key = "id"
+class VoiceCalls(ExportIncrementalWelkinHealthStream):
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "export/VOICE_CALL"
 
-class ReportsPatientProgramPhaseDistribution(WelkinHealthStream):
+class ReportPatientProgramPhaseDistribution(WelkinHealthStream):
     primary_key = "title"
 
     def path(
@@ -219,77 +183,6 @@ class ReportsPatientProgramPhaseDistribution(WelkinHealthStream):
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         data = response.json()
         yield from [record for record in data]
-
-# Basic incremental stream
-class IncrementalWelkinHealthStream(WelkinHealthStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    state_checkpoint_interval = 500
-    cursor_field = "updatedAt"
-
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-# class Employees(IncrementalWelkinHealthStream):
-#     """
-#     TODO: Change class name to match the table/data source this stream corresponds to.
-#     """
-
-#     # TODO: Fill in the cursor_field. Required.
-#     cursor_field = "start_date"
-
-#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-#     primary_key = "employee_id"
-
-#     def path(self, **kwargs) -> str:
-#         """
-#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-#         return "single". Required.
-#         """
-#         return "employees"
-
-#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-#         """
-#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-#         section of the docs for more information.
-
-#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-#         craft that specific request.
-
-#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-#         the date query param.
-#         """
-#         raise NotImplementedError("Implement stream slices or delete this method!")
-
 
 # Source
 class SourceWelkinHealth(AbstractSource):
@@ -330,7 +223,7 @@ class SourceWelkinHealth(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         try:
             args = self.convert_config2stream_args(config)
-            stream = ExportPatients(**args)
+            stream = Patients(**args)
             records = stream.read_records(sync_mode=SyncMode.full_refresh)
             next(records)
             return True, None
@@ -345,17 +238,17 @@ class SourceWelkinHealth(AbstractSource):
         args = self.convert_config2stream_args(config)
         all_streams_mapping = {
             # sorted in alphabet order
-            "export_assessments": ExportAssessments(**args),
-            "export_calendar_events": ExportCalendarEvents(**args),
-            "export_cdt_records": ExportCdtRecords(**args),
-            "export_emails": ExportEmails(**args),
-            "export_encounters": ExportEncounters(**args),
-            "export_encounter_comments": ExportEncounterComments(**args),
-            "export_encounter_dispositions": ExportEncounterDispositions(**args),
-            "export_patients": ExportPatients(**args),
-            "export_sms": ExportSms(**args),
-            "export_tasks": ExportTasks(**args),
-            "export_voice_calls": ExportVoiceCalls(**args),
-            "reports_patient_program_phase_distribution": ReportsPatientProgramPhaseDistribution(**args)
+            "assessments": Assessments(**args),
+            "calendar_events": CalendarEvents(**args),
+            "cdt_records": CdtRecords(**args),
+            "emails": Emails(**args),
+            "encounters": Encounters(**args),
+            "encounter_comments": EncounterComments(**args),
+            "encounter_dispositions": EncounterDispositions(**args),
+            "patients": Patients(**args),
+            "sms": Sms(**args),
+            "tasks": Tasks(**args),
+            "voice_calls": VoiceCalls(**args),
+            "report_patient_program_phase_distribution": ReportPatientProgramPhaseDistribution(**args)
         }
         return [stream_cls for stream_cls in all_streams_mapping.values()]
